e27053b32d89e00f0e1460ac8cd4921edfc1134c
IM-MC <matthewchung022@hotmail.com>
2017-01-14 00:35:47 -0500

    Update AssetLoader.java

--- src/com/matth/trHelpers/AssetLoader.java
+++ src/com/matth/trHelpers/AssetLoader.java
@@ -16,7 +16,7 @@ public class AssetLoader {
 
   //Hiro Run
   public static Animation<TextureRegion> hiroRunAnimation;
-  public static TextureRegion hiro1,hiro2,hiro3,hiro4,hiro5,hiro6,hiro7,hiro8;
+  public static TextureRegion hiro1,hiro2,hiro3,hiro4,hiro5,hiro6,hiro7,hiro8,hiro9,hiro10;
 
   //Hiro Jump
   public static Texture jumpTexture;
@@ -24,30 +24,34 @@ public class AssetLoader {
   public static TextureRegion hiroJ1,hiroJ2,hiroJ3,hiroJ4,hiroJ5,hiroJ6,hiroJ7;
 
   public static void load(){
-    runTexture = new Texture("kirbyrun.png");
+    runTexture = new Texture("run.png");
     runTexture.setFilter(TextureFilter.Nearest,TextureFilter.Nearest);
 
     //Run Hiro
-    hiro1 = new TextureRegion(runTexture,0,55,37,55);
+    hiro1 = new TextureRegion(runTexture,1,1,27,35);
     hiro1.flip(false,true);
-    hiro2 = new TextureRegion(runTexture,37,55,44,55);
+    hiro2 = new TextureRegion(runTexture,30,2,28,34);
     hiro2.flip(false,true);
-    hiro3 = new TextureRegion(runTexture,81,55,44,55);
+    hiro3 = new TextureRegion(runTexture,60,3,29,33);
     hiro3.flip(false,true);
-    hiro4 = new TextureRegion(runTexture,129,55,46,55);
+    hiro4 = new TextureRegion(runTexture,91,4,29,32);
     hiro4.flip(false,true);
-    hiro5 = new TextureRegion(runTexture,176,55,41,55);
+    hiro5 = new TextureRegion(runTexture,122,4,28,32);
     hiro5.flip(false,true);
-    hiro6 = new TextureRegion(runTexture,216,55,41,55);
+    hiro6 = new TextureRegion(runTexture,152,3,26,33);
     hiro6.flip(false,true);
-    hiro7 = new TextureRegion(runTexture,257,55,41,55);
+    hiro7 = new TextureRegion(runTexture,180,2,25,34);
     hiro7.flip(false,true);
-    hiro8 = new TextureRegion(runTexture,301,55,42,55);
+    hiro8 = new TextureRegion(runTexture,207,1,24,35);
     hiro8.flip(false,true);
+    hiro9 = new TextureRegion(runTexture,60,3,29,33);
+    hiro9.flip(false,true);
+    hiro10 = new TextureRegion(runTexture,1,1,27,35);
+    hiro10.flip(false,true);
 
-    TextureRegion[] run = {hiro1,hiro2,hiro3,hiro4,hiro5,hiro6,hiro7,hiro8};
-    hiroRunAnimation = new Animation<TextureRegion>(0.5f,run);
-    hiroRunAnimation.setPlayMode(Animation.PlayMode.LOOP);
+    TextureRegion[] run = {hiro2,hiro3,hiro4,hiro5,hiro6,hiro7,hiro8};
+    hiroRunAnimation = new Animation<TextureRegion>(0.06f,run);
+    hiroRunAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 
     //Jump Hiro
     jumpTexture = new Texture("kirbyjump.png");
